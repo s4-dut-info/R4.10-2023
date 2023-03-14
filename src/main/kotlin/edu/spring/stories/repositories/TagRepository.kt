697805b59aec10b9aba5142fdfc739d89aaf8944
jcheron <myaddressmail@gmail.com>
2023-03-14 13:23:46 +0100

    feat: remove stories

--- src/main/kotlin/edu/spring/stories/repositories/TagRepository.kt
+++
@@ -1,8 +0,0 @@
-package edu.spring.stories.repositories
-
-import edu.spring.stories.entities.Tag
-import org.springframework.data.repository.CrudRepository
-
-interface TagRepository:CrudRepository<Tag, Int> {
-    fun findByColor(color:String):List<Tag>
-}
