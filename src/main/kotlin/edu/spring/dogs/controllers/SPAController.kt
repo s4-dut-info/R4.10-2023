0133fd2f4800143c320fd6a522b6f3c267e9ca54
jcheron <myaddressmail@gmail.com>
2023-03-16 09:12:25 +0100

    feat: add spa methods

--- src/main/kotlin/edu/spring/dogs/controllers/SPAController.kt
+++ src/main/kotlin/edu/spring/dogs/controllers/SPAController.kt
@@ -1,6 +1,5 @@
 package edu.spring.dogs.controllers
 
-import edu.spring.dogs.entities.Master
 import edu.spring.dogs.repositories.DogRepository
 import edu.spring.dogs.repositories.MasterRepository
 import io.github.jeemv.springboot.vuejs.VueJS
@@ -16,32 +15,37 @@ import org.springframework.web.bind.annotation.RequestMapping
 class SPAController {
 
     @Autowired
-    lateinit var dogRepo: DogRepository
-
+    lateinit var dogRepository: DogRepository
     @Autowired
-    lateinit var masterRepo: MasterRepository
-
-    @Autowired
-    lateinit var vue: VueJS
+    lateinit var vue:VueJS
 
     @ModelAttribute("vue")
-    fun vue(): VueJS = vue
+    fun getVueInstance() = vue
 
     @RequestMapping(path=["/",""])
-    fun index(): String {
-        vue.addData("masters", masterRepo.findAll())
-        vue.addData("dogs", dogRepo.findByMasterIsNull())
-        vue.addData("master", Master("",""))
+    fun index():String {
+        vue.addDataRaw("masters", "[]")
+        vue.addData("dogs", dogRepository.findByMasterIsNull())
+        vue.addDataRaw("newMaster", "{}")
+
         vue.addMethod("addMaster",
-            Http.post("/masters","master",
-                JsArray.add("this.masters","master")),
+            Http.post("/masters",
+                "master",
+                JsArray.add("this.masters","master")+
+                        "this.newMaster={}",
+                "console.log('Erreur')"),
             "master")
-        vue.addMethod("remove",
+
+        vue.addMethod("removeMaster",
             Http.delete("'/masters/'+master.id",
                 JsArray.remove("this.masters","master")+
-                JsArray.addAll("this.dogs","master.dogs"),
-                "console.log('erreur sur suppression')"),
+                        JsArray.addAll("this.dogs","master.dogs"),
+                "console.log('Erreur')"),
             "master")
+
+        vue.onMounted(Http.get("/masters",
+            Http.responseArrayToArray("this.masters", "masters"),
+            "console.log('Erreur')"))
         return "/spa/index"
     }
 }
